a7bd1a878b44b6155b016405350d3e3353b270ca
Patrick Ellis <patrick@soundcloud.com>
2013-11-05 09:20:29 -0800

    remove more triggering code from arduino

--- oscilloscope_input/oscilloscope_input.ino
+++ oscilloscope_input/oscilloscope_input.ino
@@ -3,23 +3,10 @@
 #endif
 
 const int analogIn = 0;
-const int gateOut = 10;
-const int triggerOut = 11;
-
 const char beginHeader = 0xff;
 const int serialBaudRate = 28800;
-const int period = 2000;
-const int triggerDuration = 20;
-const int gateDuration = 400;
-const int triggerVoltageOn = LOW;
-const int triggerVoltageOff = HIGH;
-const int gateVoltageOn = HIGH;
-const int gateVoltageOff = LOW;
-unsigned long cyclePosition = 0;
 
 void setup() {
-  pinMode(gateOut, OUTPUT);
-  pinMode(triggerOut, OUTPUT);
   Serial.begin(serialBaudRate);
 }
 
