a974ceacc69e1c0465880720904b1d71d6acc11b
Patrick Ellis <patrick@soundcloud.com>
2013-11-20 00:16:20 +0100

    Make test more complicated

--- display/renderarea.cpp
+++ display/renderarea.cpp
@@ -19,6 +19,20 @@ RenderArea::RenderArea(QWidget *parent) : QWidget(parent) {
   setAutoFillBackground(true);
   pen = new QPen(QColor(10,10,10), 1);
 
+  RingBuffer *ring_buffer = ring_buffer_init(numSamples);
+
+  for (int i = 0; i < numSamples; i++) {
+    double sample = sin(i / (double)numSamples);
+
+    ring_buffer_push(ring_buffer, sample);
+  }
+
+  QPoint *points = (QPoint*) malloc(numSamples * sizeof(QPoint));
+  int displayHeightPixels = this->sizeHint().height();
+
+  for (int i = 0; i < numSamples; i++) {
+    points[i] = QPoint(i, (displayHeightPixels * ring_buffer_pop(ring_buffer)));
+  }
 }
 
 QSize RenderArea::minimumSizeHint(void) const {
@@ -30,28 +44,11 @@ QSize RenderArea::sizeHint(void) const {
 }
 
 void RenderArea::paintEvent(QPaintEvent *event) {
-  std::cout << "ja" << std::endl;
-  RingBuffer *ring_buffer = ring_buffer_init(numSamples);
-
-  for (int i = 0; i < numSamples; i++) {
-    double sample = sin(i / (double)numSamples);
-
-    ring_buffer_push(ring_buffer, sample);
-  }
-
-  QPoint points[numSamples];
-  int displayHeightPixels = this->sizeHint().height();
-
-  for (int i = 0; i < numSamples; i++) {
-    points[i] = QPoint(i, (displayHeightPixels * ring_buffer_get(ring_buffer, i)));
-  }
-
   QWidget::paintEvent(event);
   QPainter painter(this);
   painter.setRenderHint(QPainter::Antialiasing, true);
   painter.setPen(*(this->pen));
   painter.drawPolyline(points, numSamples);
 
-  std::cout << "jawohl" << std::endl;
-  ring_buffer_free(ring_buffer);
+  // ring_buffer_free(ring_buffer);
 }
--- display/ring_buffer_test.c
+++ display/ring_buffer_test.c
@@ -12,11 +12,13 @@ int main(int argc, char *argv[]) {
   ring_buffer_push(buffer, 0.2);
   ring_buffer_push(buffer, 0.3);
   ring_buffer_push(buffer, 0.4);
+  ring_buffer_push(buffer, 0.5);
+  ring_buffer_push(buffer, 0.6);
 
+  assert(fabs(ring_buffer_pop(buffer) - 0.6) < EPSILON);
+  assert(fabs(ring_buffer_pop(buffer) - 0.5) < EPSILON);
   assert(fabs(ring_buffer_pop(buffer) - 0.4) < EPSILON);
   assert(fabs(ring_buffer_pop(buffer) - 0.3) < EPSILON);
-  assert(fabs(ring_buffer_pop(buffer) - 0.2) < EPSILON);
-  assert(fabs(ring_buffer_pop(buffer) - 0.1) < EPSILON);
 
   return(0);
 }
