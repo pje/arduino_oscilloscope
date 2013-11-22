2357c61c6548965662d3ab7d7a468c0e48490278
Patrick Ellis <patrick@soundcloud.com>
2013-11-22 13:37:01 +0100

    [testing]

--- input/input.ino
+++ input/input.ino
@@ -1,39 +1,38 @@
 #ifndef CHAR_BIT
-  #define CHAR_BIT 8
+#define CHAR_BIT 8
 #endif
 
+const float pi = 3.141592653;
 const int analogIn = 0;
-const char beginHeader = 0xff;
-const int serialBaudRate = 28800;
+const int serialBaudRate = 9600;
+
+int i = 0;
+unsigned char j = '0';
 
 void setup() {
   Serial.begin(serialBaudRate);
 }
 
-void serialWrite(int voltage, unsigned long time) {
-
-  // byte1 : voltage
-  // byte2 : voltage
-  // byte3 : time
-  // byte4 : time
-  // byte5 : time
-  // byte6 : time
-
-  int bytesToSend = sizeof(typeof(voltage)) + sizeof(typeof(time));
-
-  int byte1 = ((voltage >> CHAR_BIT) & 0xff);
-  int byte2 = (voltage & 0xff);
-//  Serial.write(beginHeader);
-//  Serial.write(byte1);
-//  Serial.write(byte2);
-//  Serial.write('a');
-//  Serial.write('b');
-//  Serial.write('c');
-  Serial.write(time);
-  Serial.write('\n');
+void serialWriteVoltage( int voltage) {
+  byte byte1 = ((voltage >> CHAR_BIT) & 0xff);
+  byte byte2 = (voltage & 0xff);
+  byte bytes[2] = { byte1, byte2 };
+  Serial.write(bytes, 2);
 }
 
 void loop() {
-  serialWrite(analogRead(analogIn), micros());
+
+//  serialWriteVoltage(analogRead(analogIn));
+// <sin for testing>
+//  int degree = ++i;
+//  double rads = (((degree % 360) * pi) / 180.0); // (0..2pi)
+//  double sample = ((sin(rads) + 1.0) / 2.0);     // (0..1)
+//  int value = (unsigned int) (sample * 1024.0);
+//  Serial.println(value);
+//  serialWriteVoltage(value);
+// </sin>
+
+  serialWriteVoltage(++i % 1024);
+  delay(10);
 }
 
