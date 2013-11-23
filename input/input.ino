4472d5e8a5ccd908aeeba47aeeefa1126e54e041
Patrick Ellis <patrick@soundcloud.com>
2013-11-23 17:14:12 +0100

    Clarify hertz in input production

--- input/input.ino
+++ input/input.ino
@@ -5,7 +5,7 @@
 const int analogIn = 0;
 const int serialBaudRate = 9600;
 unsigned int i = 0;
-const int sample_production_rate_hertz = 10;
+const int sample_production_rate_hertz = 100;
 
 void setup() {
   Serial.begin(serialBaudRate);
@@ -22,6 +22,6 @@ void loop() {
 //  int voltage_value = analogRead(analogIn);
   int voltage_value = ++i % 1024;
   serialWriteVoltage(voltage_value);
-  delay(100 / sample_production_rate_hertz);
+  delay(1000 / sample_production_rate_hertz);
 }
 
