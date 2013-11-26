7106ae464749cde06e299b1a39ca8a732ca60917
Patrick Ellis <patrick@soundcloud.com>
2013-11-25 18:48:37 -0800

    sample_producer now expects only a single byte from arduino per sample

--- display/sample_producer.c
+++ display/sample_producer.c
@@ -11,38 +11,30 @@
 void *sample_producer_start(void *ring_buffer) {
   RingBuffer *buffer = (RingBuffer*) ring_buffer;
   int fd = -1;
-  const char *serialport = "/dev/tty.usbserial-A600afNY";
+  const char *port = "/dev/tty.usbserial-A600afNY";
   const int baudrate = 9600;
-
-  // bits_per_producer_loop = 16
-  // samples_per_second     = baudrate / bits_per_producer_loop
-  // 600                    = 9600     / 16
-  //
-  // seconds_in_backlog     = 10
-  // samples_in_backlog     = seconds_in_backlog * samples_per_second
-  // 6000                   = 10                 * 600
-
-  const int max_sample_value = 1024;
-  const int sleep_micros = 1000; // 1 millisecond
+  // const int max_sample_value = 1024;
+  const int max_sample_value = 255;
+  const int sleep_micros = 1000;
   fd = arduino_serial_port_init(port, baudrate);
   if (fd == -1) sample_producer_error("couldn't open port");
-  const size_t num_raw_samples = 2;
-  unsigned char raw_samples[2];
+  const size_t protocol_sample_frame_size = 1;
+  unsigned char raw_samples[protocol_sample_frame_size];
+
   while(1) {
     memset(raw_samples, 0, sizeof(unsigned char) * protocol_sample_frame_size);
     int read_result = arduino_serial_port_read(fd, raw_samples, protocol_sample_frame_size, sleep_micros);
     if(read_result < 0) sample_producer_error("read() returned negative value");
-    for (size_t i = 0; i < num_raw_samples; i++){
-      if (i % 2 == 1) {
-        int byte1 = (int) raw_samples[i - 1];
-        int byte2 = (int) raw_samples[i];
-        int voltage = (byte1 << 8) | byte2;
-        TYPE sample = (TYPE)(voltage / (TYPE)max_sample_value);
-        pthread_mutex_lock(buffer->elements_lock);
-        ring_buffer_push(buffer, sample);
-        pthread_mutex_unlock(buffer->elements_lock);
-      }
-    }
+    if(read_result != protocol_sample_frame_size) sample_producer_error("read() read too many bytes?");
+
+    // int byte2 = (int) raw_samples[1];
+    // int byte1 = (int) raw_samples[2];
+    // int voltage = (byte2 << 8) | byte1;
+    unsigned int voltage = (unsigned int)raw_samples[0];
+    TYPE sample = (TYPE)(voltage / (TYPE)max_sample_value);
+    pthread_mutex_lock(buffer->elements_lock);
+    ring_buffer_push(buffer, sample);
+    pthread_mutex_unlock(buffer->elements_lock);
   }
 }
 
