1e6cd6830d6ee7a4e44c8fa3d95e7115beab7479
Patrick Ellis <patrick@soundcloud.com>
2013-11-26 17:57:15 -0800

    Register a window-resize callback with GLFW
    Instead of handling it separately in the update method. This fixes that damn segfault that has been
    happening for a bit now.

--- display/main.c
+++ display/main.c
@@ -1,4 +1,4 @@
-#include <GLFW/glfw3.h>
+#include "GLFW/glfw3.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,7 +9,7 @@
 #include "sample_producer.h"
 #include "colors.h"
 
-size_t i = 0;
+unsigned long long i = 0;
 GLFWwindow* window;
 const size_t default_width = 512;
 const size_t default_height = 512;
@@ -19,6 +19,7 @@ RingBuffer *ring_buffer;
 TYPE *samples_drawable;
 size_t sizeof_samples_drawable = default_width;
 size_t sizeof_ring_buffer = 1024;
+pthread_mutex_t *samples_drawable_lock;
 
 static void error_callback(int error, const char* description) {
   fputs(description, stderr);
@@ -30,9 +31,25 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
   }
 }
 
+void window_resize_callback(GLFWwindow* window, int width, int height) {
+  current_width = width;
+  current_height = height;
+  sizeof_samples_drawable = current_width;
+  pthread_mutex_lock(samples_drawable_lock);
+  if (samples_drawable != NULL) {
+    free(samples_drawable);
+  }
+  samples_drawable = (TYPE*) calloc(sizeof_samples_drawable, sizeof(TYPE));
+  pthread_mutex_unlock(samples_drawable_lock);
+}
+
 void initialize(void) {
+  samples_drawable_lock = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+
+  glfwSetWindowSizeCallback(window, window_resize_callback);
+
   ring_buffer = ring_buffer_init(sizeof_ring_buffer);
-  samples_drawable = calloc(sizeof_samples_drawable, sizeof(TYPE));
+  samples_drawable = (TYPE*) calloc(sizeof_samples_drawable, sizeof(TYPE));
   glfwSetErrorCallback(error_callback);
   if (!glfwInit()) exit(EXIT_FAILURE);
   window = glfwCreateWindow(default_width, default_height, "oscil", NULL, NULL);
@@ -43,32 +60,22 @@ void initialize(void) {
   glfwGetWindowSize(window, (int *)&current_width, (int *)&current_height);
 }
 
-void on_resize() {
-  sizeof_samples_drawable = current_width;
-  if (samples_drawable != NULL) {
-    free(samples_drawable);
-  }
-  samples_drawable = calloc(sizeof_samples_drawable, sizeof(TYPE));
-}
-
 void update(void) {
-  glfwGetWindowSize(window, (int *)&current_width, (int *)&current_height);
-
-  // if (sizeof_samples_drawable != current_width) { on_resize(); }
-
   TYPE sample = (((++i) % current_width) / (double) current_width);
   pthread_mutex_lock(ring_buffer->elements_lock);
   ring_buffer_push(ring_buffer, sample);
   pthread_mutex_unlock(ring_buffer->elements_lock);
 
   pthread_mutex_lock(ring_buffer->elements_lock);
+  pthread_mutex_lock(samples_drawable_lock);
   ring_buffer_get_n(ring_buffer, sizeof_samples_drawable, samples_drawable);
+  pthread_mutex_unlock(samples_drawable_lock);
   pthread_mutex_unlock(ring_buffer->elements_lock);
 
   glClear(GL_COLOR_BUFFER_BIT);
   glClearColor(base03[0], base03[1], base03[2], base03[3]);
   glColor4fv(yellow);
-
+  // pthread_mutex_lock(samples_drawable_lock);
   glBegin(GL_LINE_STRIP);
   for (size_t j = 0; j < sizeof_samples_drawable; j++) {
     TYPE sample = samples_drawable[sizeof_samples_drawable - j];
@@ -77,7 +84,7 @@ void update(void) {
     glVertex2i(gl_x_coord, gl_y_coord);
   }
   glEnd();
-  glFlush();
+  // pthread_mutex_unlock(samples_drawable_lock);
   glfwSwapBuffers(window);
   glfwPollEvents();
 }
