030b7816f8ad094b150f4f18c07d1f32fa033cc4
Patrick Ellis <patrick@soundcloud.com>
2013-11-29 20:44:08 -0800

    Remove unused forward struct declaration from signal_source.h

--- consumer/signal_source.h
+++ consumer/signal_source.h
@@ -1,8 +1,6 @@
 #ifndef SIGNAL_SOURCE_H
 #define SIGNAL_SOURCE_H
 
-struct RingBuffer;
-
 void *signal_source_start(void *ring_buffer);
 _Noreturn void signal_source_error(char* msg, int fd);
 void signal_source_print_bytes(size_t size, void const * const ptr);
