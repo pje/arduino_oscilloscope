2f3c4ff991b01925cce2f42dfc5b99d8b61d1ce5
Patrick Ellis <patrick@soundcloud.com>
2013-11-29 19:10:37 -0800

    Move main.c static variable declarations/definitions into main.h

--- consumer/main.c
+++ consumer/main.c
@@ -12,21 +12,6 @@
 #include "colors.h"
 #include "main.h"
 
-static GLFWwindow* window;
-static const size_t max_window_width = 2048;
-static const size_t default_window_width = 512;
-static const size_t default_window_height = 512;
-static size_t current_width = default_window_width;
-static size_t current_height = default_window_height;
-static RingBuffer *ring_buffer;
-static TYPE *samples_drawable;
-static size_t sizeof_samples_drawable = default_window_width;
-static size_t sizeof_ring_buffer = 2048;
-static pthread_mutex_t *samples_drawable_lock;
-static pthread_t *producer_thread;
-static const size_t mutex_attempts = 100;
-static const size_t grid_divisions_vertical = 5;
-
 static void error_callback(int error, const char* description) {
   fputs(description, stderr);
 }
