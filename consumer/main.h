687208337383561bd027abcd8025dc8372f49753
Patrick Ellis <patrick@soundcloud.com>
2013-11-29 19:29:34 -0800

    Initialize all main.h pointers to NULL

--- consumer/main.h
+++ consumer/main.h
@@ -4,17 +4,17 @@
 struct GLFWwindow;
 struct RingBuffer;
 
-static GLFWwindow* window;
+static GLFWwindow* window = NULL;
 static const size_t max_window_width = 2048;
 static const size_t default_window_width = 512;
 static const size_t default_window_height = 512;
 static size_t current_width = default_window_width;
 static size_t current_height = default_window_height;
-static RingBuffer *ring_buffer;
-static TYPE *samples_drawable;
 static size_t sizeof_ring_buffer = 2048;
-static pthread_mutex_t *samples_drawable_lock;
-static pthread_t *producer_thread;
+static RingBuffer *ring_buffer = NULL;
+static TYPE *samples_drawable = NULL;
+static pthread_mutex_t *samples_drawable_lock = NULL;
+static pthread_t *producer_thread = NULL;
 static const size_t mutex_attempts = 100;
 static const unsigned short int grid_divisions_vertical = 5;
 static const unsigned short int grid_dot_width = 2;
